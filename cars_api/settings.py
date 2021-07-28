a1e331494b3d2775db8a97262f4ff00dc20ff455
hevertogomes <heverton.gomes2018@gmail.com>
2021-07-27 22:38:56 -0300

    feat: Adição de configuração do banco e migrations

--- cars_api/settings.py
+++ cars_api/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'cars_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Postgres2018!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
