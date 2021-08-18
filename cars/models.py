dabbca488a2c51f47a8bd0d98807f8787ec055cd
Heverton Gomes <heverton.gomes2018@gmail.com>
2021-08-18 20:39:48 -0300

    Update verbose_name models

--- cars/models.py
+++ cars/models.py
@@ -16,5 +16,5 @@ class Carro(models.Model):
     peso = models.IntegerField('Peso')
     aceleracao = models.DecimalField('Acelaracão', max_digits=10, decimal_places=2)
     data_fabricacao = models.DateTimeField('Data de fabricação')
-    origem = models.CharField("Origem", max_length=15)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    origem = models.CharField('Origem', max_length=15)
+    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)
